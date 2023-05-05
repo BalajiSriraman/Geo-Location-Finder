18ed88997caa724c99918517d806d6275a26424b
2005balaji <balajisr000@gmail.com>
2023-05-05 21:30:40 +0530

    debug

--- server.js
+++ server.js
@@ -9,18 +9,21 @@ app.get('/', async (req, res) => {
     const ipAddress = req.ip.split(':').pop();
 
     const { ip } = ipAddress;
-    const url = `https://api.ipgeolocation.io/ipgeo?apiKey=775c5318c1fd479697017eb228ff9ef7&ip`;
+    const url = `https://api.ipgeolocation.io/ipgeo?apiKey=775c5318c1fd479697017eb228ff9ef7&ip=${ip}`;
 
     try {
         const response = await axios.get(url);
         const data = response.data;
         res.json(data);
     } catch (error) {
-        res.status(500).send(error);
+        // res.status(500).send(error);
+        res.send(ipAddress)
+        console.error("error");
     }
 });
 
-app.listen(80, () => {
-    console.log('Server listening on port 80');
+const port = 3000
+app.listen(port, () => {
+    console.log('Server listening on port ' + port);
 });
 
