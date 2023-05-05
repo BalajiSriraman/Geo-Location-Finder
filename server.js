f05393d108286f12f6fe0a38ffc84546156576c0
2005balaji <balajisr000@gmail.com>
2023-05-05 16:44:06 +0530

    running natively without libraries

--- server.js
+++ server.js
@@ -4,20 +4,10 @@ const axios = require('axios');
 const app = express();
 
 
-const requestIp = require('request-ip');
-
-app.use(requestIp.mw());
-
-app.get('/', function (req, res) {
-    const ipAddress = req.clientIp;
-    // do something with the ipAddress
-    res.send('Your IP address is ' + ipAddress);
-});
-
 app.get('/', async (req, res) => {
 
-    const ipAddress = req.clientIp;
-    // do something with the ipAddress
+    const ipAddress = req.ip.split(':').pop();
+
     const { ip } = ipAddress;
     const url = `https://api.ipgeolocation.io/ipgeo?apiKey=775c5318c1fd479697017eb228ff9ef7&ip=${ip}`;
 
@@ -26,11 +16,11 @@ app.get('/', async (req, res) => {
         const data = response.data;
         res.json(data);
     } catch (error) {
-        res.send(ipAddress)
         res.status(500).send(error);
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+app.listen(80, () => {
+    console.log('Server listening on port 80');
 });
+
